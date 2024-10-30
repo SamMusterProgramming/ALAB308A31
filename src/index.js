1c1448704fe082a5590629606a848bec3f9a72a3
samcoeur <samcoeur2013@gmail.com>
2024-10-29 23:04:16 -0400

    worked on async function

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 
+function validateId(id) {
+  return !isNaN(id);
+}
+
 async function getUserDataUsingThen(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
-  
-  return   await  central(id)
-    .then((returneDB) => dbs[returneDB](id))
-    .then(
-    //concatenate id +  two userinfo objects fecthed from one of the dbs + vault
-     result => vault(id).then(result1 => result1 = {id:id,...result1,...result})
-    ).
-    catch(error => console.error(error))
+  try {
+    if(validateId(id)){
+      return   await   central(id)
+      .then((returneDB) => dbs[returneDB](id))
+      .then(
+      //concatenate id +  two userinfo objects fecthed from one of the dbs + vault
+       result => vault(id).then(result1 => result1 = {id:id,...result1,...result})
+      ).
+      catch(error => console.error(error))
+    }else throw new Error("invalid entry , must enter valid number")
+  } catch (error) {
+    console.log(error)
+  }
+ 
     
 }
 
@@ -25,21 +35,25 @@ async function getUserDataUsingAsync(id) {
     db2: db2,
     db3: db3,
   };
-  
+
+  const dbValue =  await central(id);
+  const userInfoDB = await dbs[dbValue](id) 
+  const userInfoVault = await vault(id);
+  return {...userInfoVault,...userInfoDB}
 }
 
-const div = document.querySelector("#app");
-const input = document.createElement("input");
-input.setAttribute("placeholder", "enter Id here");
-const submitButton = document.createElement("button");
-submitButton.textContent = "Submitt";
-div.appendChild(input);
-div.appendChild(submitButton);
+// const div = document.querySelector("#app");
+// const input = document.createElement("input");
+// input.setAttribute("placeholder", "enter Id here");
+// const submitButton = document.createElement("button");
+// submitButton.textContent = "Submitt";
+// div.appendChild(input);
+// div.appendChild(submitButton);
   
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-   div.textContent = `${getUserData(5)}`;
-});
+// submitButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   div.textContent = `${getUserData(5)}`;
+// });
 
 
-//  (console.log( await getUserDataUsingThen(5)))
+ (console.log( await getUserDataUsingThen("10")))
